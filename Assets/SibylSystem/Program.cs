499f520b8cec9a0b64f2af8be4fcdb16e3e08b4a
purerosefallen <78877@qq.com>
2019-08-07 18:09:36 +0800

    version

--- Assets/SibylSystem/Program.cs
+++ Assets/SibylSystem/Program.cs
@@ -338,7 +338,7 @@ public class Program : MonoBehaviour
 
 #elif UNITY_IPHONE //iPhone
         string GamePaths = Application.persistentDataPath + "/ygopro2/";
-        if (!File.Exists(GamePaths + "updates/versionB-2.txt"))
+        if (!File.Exists(GamePaths + "updates/versionB-3.txt"))
         {
             string filePath = Application.streamingAssetsPath + "/ygopro2-data.zip";
             ExtractZipFile(System.IO.File.ReadAllBytes(filePath), GamePaths);
