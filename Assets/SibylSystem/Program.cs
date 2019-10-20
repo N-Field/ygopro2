68e651bf3d542f31e4458c9225193474df719783
purerosefallen <78877@qq.com>
2019-10-20 14:52:50 +0800

    version

--- Assets/SibylSystem/Program.cs
+++ Assets/SibylSystem/Program.cs
@@ -1116,7 +1116,7 @@ public class Program : MonoBehaviour
 
         string FPS = m_FPS.ToString();
         try { FPS = FPS.Substring(0, 5); } catch{}
-        GUI.Label(new Rect(10, 5, 200, 200), "[Ver 1.034.B-4] " + "FPS: " + FPS);
+        GUI.Label(new Rect(10, 5, 200, 200), "[Ver 1.034.B-4.1] " + "FPS: " + FPS);
     }
 
     void Update()
