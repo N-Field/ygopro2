e0d529df386dfc69f5a56630f92ff2f9f4a3685f
purerosefallen <78877@qq.com>
2020-01-25 21:34:31 +0800

    missing

---
+++ gitlab-ci/build.bat
@@ -0,0 +1,14 @@
+@echo off
+
+mkdir output
+Unity -batchmode -nographics -silent-crashes -projectPath %cd% -logFile build.log -executeMethod BuildHelper.Build -quit
+cat build.log
+
+sed -i '/>UIStatusBarStyle</i\    <key>UIFileSharingEnabled</key>\n    <true />\n    <key>LSSupportsOpeningDocumentsInPlace</key>\n    <true />' output/Info.plist
+
+mkdir dist
+cd output
+7z a -mx9 ../dist/KoishiPro2-src.7z *
+cd ..
+
+aws s3 --endpoint=https://minio.mycard.moe:9000 cp dist/KoishiPro2-src.7z s3://mycard/download/
