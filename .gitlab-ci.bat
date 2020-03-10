2ba0dfc7f951b89579dc621af4ce369fc6dcecdc
purerosefallen <78877@qq.com>
2020-03-10 08:43:44 +0800

    error handle

--- .gitlab-ci.bat
+++ .gitlab-ci.bat
@@ -5,6 +5,7 @@ set UNITY_PATH="C:\Program Files\Unity\Editor\Unity.exe"
 mkdir output
 mkdir dist
 %UNITY_PATH% -batchmode -nographics -silent-crashes -projectPath %cd% -logFile dist/build.log -executeMethod BuildHelper.Build -quit
+if %errorlevel% neq 0 exit /b %errorlevel%
 cat build.log
 
 bash -c "sed -i '/>UIStatusBarStyle</i\    <key>UIFileSharingEnabled</key>\n    <true />\n    <key>LSSupportsOpeningDocumentsInPlace</key>\n    <true />' output/Info.plist"
