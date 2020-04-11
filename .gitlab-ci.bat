3f6fbab813661e166fa6c4723b8c997d7dcfcccb
purerosefallen <78877@qq.com>
2020-04-11 14:46:58 +0800

    update ci

--- .gitlab-ci.bat
+++ .gitlab-ci.bat
@@ -3,7 +3,7 @@
 set UNITY_PATH="C:\Program Files\Unity\Editor\Unity.exe"
 
 mkdir output
-mkdir dist
+mkdir KoishiPro2-src
 %UNITY_PATH% -batchmode -nographics -silent-crashes -projectPath %cd% -logFile dist/build.log -executeMethod BuildHelper.Build -quit
 if %errorlevel% neq 0 exit /b %errorlevel%
 cat dist/build.log
@@ -11,7 +11,5 @@ cat dist/build.log
 bash -c "sed -i '/>UIStatusBarStyle</i\    <key>UIFileSharingEnabled</key>\n    <true />\n    <key>LSSupportsOpeningDocumentsInPlace</key>\n    <true />' output/Info.plist"
 
 cd output
-7z a -mx9 ../dist/KoishiPro2-src.7z *
+7z a -mx9 ../KoishiPro2-src/KoishiPro2-src.7z *
 cd ..
-
-aws s3 --endpoint=https://minio.mycard.moe:9000 sync dist s3://mycard/koishipro2
