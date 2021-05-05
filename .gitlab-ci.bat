95570520ab7e1b5a78d9e01ed6ea6ddeff8b3e5c
purerosefallen <78877@qq.com>
2021-05-05 10:37:48 +0800

    some patches for generated libs

--- .gitlab-ci.bat
+++ .gitlab-ci.bat
@@ -3,13 +3,8 @@
 set UNITY_PATH="C:\Program Files\Unity\Editor\Unity.exe"
 
 mkdir output
-mkdir KoishiPro2-src
-%UNITY_PATH% -batchmode -nographics -silent-crashes -projectPath %cd% -logFile KoishiPro2-src/build.log -executeMethod BuildHelper.Build -quit
+mkdir dist
+%UNITY_PATH% -batchmode -nographics -silent-crashes -projectPath %cd% -logFile dist/build.log -executeMethod BuildHelper.Build -quit
 if %errorlevel% neq 0 exit /b %errorlevel%
-cat KoishiPro2-src/build.log
-
-bash -c "sed -i '/>UIStatusBarStyle</i\    <key>UIFileSharingEnabled</key>\n    <true />\n    <key>LSSupportsOpeningDocumentsInPlace</key>\n    <true />' output/Info.plist"
-
-cd output
-7z a -mx9 ../KoishiPro2-src/KoishiPro2-src.7z *
-cd ..
+cat dist/build.log
+mv output dist/src
---
+++ patches.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+MM_PATH="./misc/URLUtility.mm"
+IPHONE_SDK_PATH="/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk"
+
+_run_for_arch() {
+	TARGET_ARCH=$1
+	OUTPUT_PATH=$2
+	echo "Running for $TARGET_ARCH to $OUTPUT_PATH"
+	lipo ./Libraries/libiPhone-lib.a -thin "$TARGET_ARCH" -output "$OUTPUT_PATH"
+	ar -d "$OUTPUT_PATH" URLUtility.o
+	clang -c "$MM_PATH" -arch "$TARGET_ARCH" -isysroot "$IPHONE_SDK_PATH"
+	ar -q "$OUTPUT_PATH" ./URLUtility.o
+	rm -f ./URLUtility.o
+}
+
+mkdir /tmp/tmp-libs
+_run_for_arch arm64 /tmp/tmp-libs/libiPhone-lib-arm64.a
+_run_for_arch armv7 /tmp/tmp-libs/libiPhone-lib-armv7.a
+_run_for_arch armv7s /tmp/tmp-libs/libiPhone-lib-armv7s.a
+lipo -create /tmp/tmp-libs/libiPhone-lib-arm64.a /tmp/tmp-libs/libiPhone-lib-armv7.a /tmp/tmp-libs/libiPhone-lib-armv7s.a -output ./Libraries/libiPhone-lib-new.a
+
+# patch -p1 < ./misc/patches/iPhone_Sensors.mm.patch
+# echo '#define UNITY_USES_LOCATION 0' >> ./Classes/Preprocessor.h
